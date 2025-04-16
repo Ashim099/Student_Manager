44ace08adfd9ce77eabbf29d60ad5b90a254beac
Ashim099 <ashimpoudel1357@gmail.com>
2025-04-16 14:06:40 +0545

    Fixed superuser role and admin access, updated CustomAdminSite, tested admin features

--- StudentManager/urls.py
+++ StudentManager/urls.py
@@ -3,14 +3,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.admin.sites import AdminSite
-from django.shortcuts import redirect 
+from django.shortcuts import redirect
+from django.contrib import messages
 
 class CustomAdminSite(AdminSite):
     def login(self, request, extra_context=None):
-        # Redirect to /admin/dashboard/ after login
         if request.method == 'POST' and request.user.is_authenticated:
-            return redirect('admin_dashboard')
+            # Allow superusers or users with role='admin' to access the dashboard
+            if request.user.is_superuser or request.user.role == 'admin':
+                return redirect('admin_dashboard')
+            else:
+                messages.error(request, 'You do not have permission to access the admin dashboard.')
+                return redirect('homepage')
         return super().login(request, extra_context)
+
+    def has_permission(self, request):
+        return request.user.is_active and (request.user.is_superuser or request.user.role == 'admin')
 
 admin.site = CustomAdminSite(name='custom_admin')
 
